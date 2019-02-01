164e0522766e96efaaa8a86638ca1c29b0742396
Stephen Latham <ste.latham@gmail.com>
2019-02-01 15:05:36 +0000

    Added a point object factory function

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // del - used for devel
-const rectangle = require('./lib/Rectangle')
+const rectangle = require('./lib/Rectangle');
+const point = require('./lib/Point');
 
 let r1 = new rectangle;
 
@@ -13,3 +14,8 @@ r2.x = 10;
 r2.y = 10;
 r2.w = 50;
 r2.h = 50;
+
+const p1 = new point({x:1,y:2,data:1234});
+const p2 = new point({x:5,y:3,data:9999});
+console.log(p1);
+console.log(p2);
---
+++ src/Point.js
@@ -0,0 +1,6 @@
+module.exports = ({x, y, data}) => ({ // using arrow function explicit return feature
+    x, y, data  // using object destructuring
+});
+
+// TODO rewrite Rectangle in this format
+
