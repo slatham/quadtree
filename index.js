277415c969f3cefb95969e6619be42841ccc653b
Stephen Latham <slatham@Stephens-iMac.local>
2019-02-08 19:47:33 +0000

    Some progress on Quadtree

--- index.js
+++ index.js
@@ -6,37 +6,52 @@ const util = require('util');
 
 const r1 = Rectangle({x:0,y:0,w:100,h:100});
 //const r2 = Rectangle({x:10,y:10,w:50,h:50});
-const p1 = Point({x:1,y:2,data:1234});
-const p2 = Point({x:5,y:3,data:9999});
-const p3 = Point({x:6,y:7,data:9999});
-const p4 = Point({x:3,y:6,data:9999});
-const p5 = Point({x:52,y:6,data:9999});
-const p6 = Point({x:55,y:6,data:9999});
-const p7 = Point({x:52,y:6,data:9999});
-const p8 = Point({x:52,y:6,data:9999});
-const p9 = Point({x:90,y:6,data:9999});
-const p10 = Point({x:99,y:6,data:9999});
+const p1 = Point({x:1,y:2,data:'point1'});
+const p2 = Point({x:5,y:3,data:'point2'});
+const p3 = Point({x:6,y:7,data:'point3'});
+const p4 = Point({x:3,y:6,data:'point4'});
+const p5 = Point({x:52,y:6,data:'point5'});
+const p6 = Point({x:55,y:6,data:'point6'});
+const p7 = Point({x:52,y:6,data:'point7'});
+const p8 = Point({x:52,y:6,data:'point8'});
+const p9 = Point({x:90.1,y:6,data:'point9'});
+const p10 = Point({x:99.6,y:6,data:'point10'});
+const p11 = Point({x:52.5,y:6,data:'point11'});
+const p12 = Point({x:53.3,y:6,data:'point12'});
+const p13 = Point({x:51.5,y:6,data:'point13'});
+const p14 = Point({x:56,y:6,data:'point14'});
+const p15 = Point({x:97,y:6,data:'point15'});
+const p16 = Point({x:98,y:6,data:'point16'});
 
 //console.log(p1);
 //console.log(p2);
 //console.log(r1);
 //console.log(r2);
 
-const q1 = Quadtree(r1,3);
+const q1 = Quadtree(r1,5);
 
-q1.insertPoint(p1);
-console.log(q1)
+
+
 q1.insertPoint(p2);
-console.log(q1)
+
 q1.insertPoint(p3);
-console.log(q1)
+
 q1.insertPoint(p4);
-console.log(q1)
-q1.insertPoint(p5);
-q1.insertPoint(p6);
-q1.insertPoint(p7);
-q1.insertPoint(p8);
-q1.insertPoint(p9);
-q1.insertPoint(p10);
+
+
+ q1.insertPoint(p6);
+ q1.insertPoint(p7);
+ q1.insertPoint(p8);
+ q1.insertPoint(p9);
+ q1.insertPoint(p10);
+ q1.insertPoint(p11);
+ q1.insertPoint(p12);
+ q1.insertPoint(p13);
+ q1.insertPoint(p14);
+ q1.insertPoint(p15);
+ q1.insertPoint(p5);
+ q1.insertPoint(p1);
+
+
 console.log('-----------------------------------------')
 console.log(util.inspect(q1, {showHidden:false, depth:null}));
--- src/Quadtree.js
+++ src/Quadtree.js
@@ -3,8 +3,8 @@ const Rectangle = require('./Rectangle');
 const Quadtree = (boundingBox, maxPointsPerNode) => {
     boundingBox;
     maxPointsPerNode;
-    const points = [];    // array to host the points
-    const childNodes = [];   // child nodes
+    let points = [];    // array to host the points
+    let childNodes = [];   // child nodes
     // a node is either a leaf - i.e. no children
     // 
     // a branch -  i.e has 4 children
@@ -35,26 +35,42 @@ const Quadtree = (boundingBox, maxPointsPerNode) => {
             const y = boundingBox.y;
             const w = boundingBox.w / 2;
             const h = boundingBox.h / 2;
-            const ne = new Quadtree(new Rectangle({x:x+w,y:y+h,w:w,h:h}),2)
-            const nw = new Quadtree(new Rectangle({x:x,y:y+h,w:w,h:h}),2)
-            const se = new Quadtree(new Rectangle({x:x+w,y:y,w:w,h:h}),2)
-            const sw = new Quadtree(new Rectangle({x:x,y:y,w:w,h:h}),2)
+            const ne = new Quadtree(new Rectangle({x:x+w,y:y+h,w:w,h:h}),maxPointsPerNode)
+            const nw = new Quadtree(new Rectangle({x:x,y:y+h,w:w,h:h}),maxPointsPerNode)
+            const se = new Quadtree(new Rectangle({x:x+w,y:y,w:w,h:h}),maxPointsPerNode)
+            const sw = new Quadtree(new Rectangle({x:x,y:y,w:w,h:h}),maxPointsPerNode)
             childNodes.push(ne)
             childNodes.push(nw)
             childNodes.push(se)
             childNodes.push(sw);
-            insertPoint(point);
+            
+            points.push(point);
+            //console.log(points)
+            points.forEach(p => {
+                
+                insertPoint(p)
+            })
+            console.log(points)
+            points.splice(0,maxPointsPerNode+1);
+
+            
+            //insertPoint(point);
+   
             return
         }
         // if is not a leaf node, call insert on children
         childNodes.forEach(el=> {
             el.insertPoint(point)
+
         });
+        
 
-    }
-
-    // split the quadtree
-    const split = (boxToSplit) => {
+        //
+        //points.splice(0,1);
+        // console.log(points)
+        // points = [];
+        // console.log(points)
+        //return
 
     }
 
