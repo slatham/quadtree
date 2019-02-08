63900272c40428af3478297cfbd7eaa8837f0ce4
Stephen Latham <ste.latham@gmail.com>
2019-02-08 12:19:11 +0000

    Started work on the quadtree section

--- index.js
+++ index.js
@@ -1,12 +1,42 @@
 // del - used for devel
 const Rectangle = require('./lib/Rectangle');
 const Point = require('./lib/Point');
+const Quadtree = require('./lib/Quadtree');
+const util = require('util');
 
 const r1 = Rectangle({x:0,y:0,w:100,h:100});
-const r2 = Rectangle({x:10,y:10,w:50,h:50});
+//const r2 = Rectangle({x:10,y:10,w:50,h:50});
 const p1 = Point({x:1,y:2,data:1234});
 const p2 = Point({x:5,y:3,data:9999});
-console.log(p1);
-console.log(p2);
-console.log(r1);
-console.log(r2);
+const p3 = Point({x:6,y:7,data:9999});
+const p4 = Point({x:3,y:6,data:9999});
+const p5 = Point({x:52,y:6,data:9999});
+const p6 = Point({x:55,y:6,data:9999});
+const p7 = Point({x:52,y:6,data:9999});
+const p8 = Point({x:52,y:6,data:9999});
+const p9 = Point({x:90,y:6,data:9999});
+const p10 = Point({x:99,y:6,data:9999});
+
+//console.log(p1);
+//console.log(p2);
+//console.log(r1);
+//console.log(r2);
+
+const q1 = Quadtree(r1,3);
+
+q1.insertPoint(p1);
+console.log(q1)
+q1.insertPoint(p2);
+console.log(q1)
+q1.insertPoint(p3);
+console.log(q1)
+q1.insertPoint(p4);
+console.log(q1)
+q1.insertPoint(p5);
+q1.insertPoint(p6);
+q1.insertPoint(p7);
+q1.insertPoint(p8);
+q1.insertPoint(p9);
+q1.insertPoint(p10);
+console.log('-----------------------------------------')
+console.log(util.inspect(q1, {showHidden:false, depth:null}));
---
+++ src/Quadtree.js
@@ -0,0 +1,69 @@
+const Rectangle = require('./Rectangle');
+// factory function with object destructuring
+const Quadtree = (boundingBox, maxPointsPerNode) => {
+    boundingBox;
+    maxPointsPerNode;
+    const points = [];    // array to host the points
+    const childNodes = [];   // child nodes
+    // a node is either a leaf - i.e. no children
+    // 
+    // a branch -  i.e has 4 children
+
+    // insert a point into the quadtree
+    const insertPoint = point => {
+        // check if valid point - within bounding box
+        if (!boundingBox.containsPoint(point)) {
+            return false
+        } 
+        
+        // if isLeaf - i.e. no children and not full
+        if (points.length < maxPointsPerNode && childNodes.length === 0){
+
+            // insert Point
+            points.push(point);
+            return
+        }
+
+
+        // if is a leaf node but full subdivide
+              // split the bounding box into 4
+              // crate 4 new quadtrees with the 4 new boxes
+              // and set them as child nodes
+        if(childNodes.length === 0){
+
+            const x = boundingBox.x;
+            const y = boundingBox.y;
+            const w = boundingBox.w / 2;
+            const h = boundingBox.h / 2;
+            const ne = new Quadtree(new Rectangle({x:x+w,y:y+h,w:w,h:h}),2)
+            const nw = new Quadtree(new Rectangle({x:x,y:y+h,w:w,h:h}),2)
+            const se = new Quadtree(new Rectangle({x:x+w,y:y,w:w,h:h}),2)
+            const sw = new Quadtree(new Rectangle({x:x,y:y,w:w,h:h}),2)
+            childNodes.push(ne)
+            childNodes.push(nw)
+            childNodes.push(se)
+            childNodes.push(sw);
+            insertPoint(point);
+            return
+        }
+        // if is not a leaf node, call insert on children
+        childNodes.forEach(el=> {
+            el.insertPoint(point)
+        });
+
+    }
+
+    // split the quadtree
+    const split = (boxToSplit) => {
+
+    }
+
+
+
+    return {boundingBox, insertPoint, points, childNodes}
+
+
+
+}
+
+module.exports = Quadtree;
